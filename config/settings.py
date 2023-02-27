74290465afca1b93010f176804112d9079c2295b
Your Name <you@example.com>
2023-02-27 15:23:09 +0100

    made some changes

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t((y3zp)2t=ahdz80$qanxo*0_4u10eqa8oi_&4n+)uwl_dokb'
+from fb_login.secret_settings import SECRET_KEY
+secret_key = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -138,12 +139,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# from django.contrib.messages import constants as messages
-# MESSAGE_TAGS = {
-#     messages.DEBUG: 'alert-info',
-#     messages.INFO: 'alert-info',
-#     messages.SUCCESS: 'alert-success',
-#     messages.WARNING: 'alert-warning',
-#     messages.ERROR: 'alert-danger',
-# }
+
 PYTHON_VERSION = 3.7
---
+++ fb_login/secret_settings.py
@@ -0,0 +1,1 @@
+SECRET_KEY = 'django-insecure-t((y3zp)2t=ahdz80$qanxo*0_4u10eqa8oi_&4n+)uwl_dokb'
